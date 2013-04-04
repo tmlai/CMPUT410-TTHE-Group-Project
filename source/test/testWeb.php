2b61da6e2a536f3bdc333466b4055d15ce1cdaea
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-04 13:09:58 -0600

    url change

--- source/test/testWeb.php
+++ source/test/testWeb.php
@@ -5,7 +5,7 @@
 			function testGetAll() {
 				var xmlhttp=new XMLHttpRequest();
 				//send ajax request
-				xmlhttp.open('GET', 'http://cs410.cs.ualberta.ca:41041/CMPUT410-TTHE-Group-Project/source/controller/webservices.php', 'true');
+				xmlhttp.open('GET', '/CMPUT410-TTHE-Group-Project/source/controller/webservices.php', 'true');
 				xmlhttp.send();
 			}
 		</script>
