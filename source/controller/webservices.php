0afc6b4afee5eaa1a9795e20fd9456186ccafbc3
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-04 13:01:10 -0600

    testing

---
+++ source/controller/webservices.php
@@ -0,0 +1,18 @@
+<?php
+namespace model;
+include_once '../model/DbLayer.php';
+
+$request_method = strtolower($_SERVER['REQUEST_METHOD']);
+switch($request_method) {
+	case 'get':
+		//if id is passed (GET /products/:id)
+		//else (GET /products)
+		if(!empty($_GET)) {
+			$id = $_GET['id'];
+		} else {
+			$prodArray = getProductsInStock();
+			echo json_encode($prodArray);
+		}
+	case 'post':
+}
+?>
---
+++ source/test/testWeb.php
@@ -0,0 +1,13 @@
+<html>
+	<header>
+		<title> testWeb.php </title>
+		<script>
+			function testGetAll() {
+			
+			}
+		</script>
+	</header>
+	<body>
+		<input type="button" onClick="testGetAll()">Get All</input>
+	</body>
+</html>
