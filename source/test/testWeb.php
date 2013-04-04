7c4dd7db0bd3397e89dd574c0095fd86a7523705
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-04 13:07:59 -0600

    added test ajax call

--- source/test/testWeb.php
+++ source/test/testWeb.php
@@ -3,7 +3,10 @@
 		<title> testWeb.php </title>
 		<script>
 			function testGetAll() {
-			
+				var xmlhttp=new XMLHttpRequest();
+				//send ajax request
+				xmlhttp.open('GET', 'http://cs410.cs.ualberta.ca:41041/CMPUT410-TTHE-Group-Project/source/controller/webservices.php', 'true');
+				xmlhttp.send();
 			}
 		</script>
 	</header>
