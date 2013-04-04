3c3f38825edfaaf93a68cd70d423b98dcccfe4f4
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-04 13:35:52 -0600

    added try catch

--- source/controller/webservices.php
+++ source/controller/webservices.php
@@ -2,27 +2,21 @@
 namespace controller;
 include_once '../model/DbLayer.php';
 
-$request_method = strtolower($_SERVER['REQUEST_METHOD']);
-echo $request_method;
-switch($request_method) {
-	case 'get':
-		//if id is passed (GET /products/:id)
-		//else (GET /products)
-		if(!empty($_GET)) {
-			$id = $_GET['id'];
-		} else {
-			// $prodArray = getProductsInStock();
-			// echo json_encode($prodArray);
-		}
-	case 'post':
+try {
+    $request_method = strtolower($_SERVER['REQUEST_METHOD']);
+	switch($request_method) {
+		case 'get':
+			//if id is passed (GET /products/:id)
+			//else (GET /products)
+			if(!empty($_GET)) {
+				$id = $_GET['id'];
+			} else {
+				// $prodArray = getProductsInStock();
+				// echo json_encode($prodArray);
+			}
+		case 'post':
+	}
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
 ?>
-<html>
-<body>
-<?
-// $prodArray = getProductsInStock();
-// echo json_encode($prodArray);
-// var_dump($prodArray);
-?>
-</body>
-</html>
