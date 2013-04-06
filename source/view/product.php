fefbdfe380b9783c2dae0a73b7fbdd7223090ed4
Tully Johnson <tullyjohnson@gmail.com>
2013-04-06 17:03:58 -0600

    product update6

--- source/view/product.php
+++ source/view/product.php
@@ -47,6 +47,107 @@ $product = json_decode($product, true);
                     echo $product['desc'];
                   ?>
                 </div>
+                <hr>
+                <div class="hero-unit">
+            <h1>Category Name</h1>
+            <p>This is a category description.</p>
+        </div>
+        <div class="container" style="width:100%; height:300px; position:relative; 
+        bottom:0px; overflow:auto;">
+                        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th><!-- placeholder --></th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Availability</th>
+                    <th>Weight</th>
+                    <th>Name</th>
+                    <th>Code</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+              <div id="resultsDiv">
+                <!-- product code javascript function will return the product code
+                for the query of the database for creating product.php-->
+                <tr onclick="location.href='./product.php?id=1'">
+                    <td>
+                      <img src="" alt="" width="50" height="50">
+                    </td>
+                    <td>$1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1 kg</td>
+                    <td>1name</td>
+                    <td>c1</td>
+                    <td>This is a description...</td>
+                    <td>
+                        <button id="p1" style="position:relative; right:0px;"
+                        class="btn pull-right">
+                            View Product
+                        </button>
+                    </td>
+                </tr>
+                <tr onclick="location.href='./product.php?id=2'">
+                    <td>
+                      <img src="" alt="" width="50" height="50">
+                    </td>
+                    <td>$2</td>
+                    <td>2</td>
+                    <td>2</td>
+                    <td>2 kg</td>
+                    <td>2name</td>
+                    <td>c2</td>
+                    <td>This is a description...</td>
+                    <td>
+                        <button id="p2" style="position:relative; right:0px;"
+                        class="btn pull-right">
+                            View Product
+                        </button>
+                    </td>
+                </tr>
+                <tr onclick="location.href='./product.php?id=3'">
+                    <td>
+                      <img src="" alt="" width="50" height="50">
+                    </td>
+                    <td>$3</td>
+                    <td>3</td>
+                    <td>3</td>
+                    <td>3 kg</td>
+                    <td>3name</td>
+                    <td>c3</td>
+                    <td>This is a description...</td>
+                    <td>
+                        <button id="p3" style="position:relative; right:0px;"
+                        class="btn pull-right">
+                            View Product
+                        </button>
+                    </td>
+                </tr>
+                <tr onclick="location.href='./product.php?id=4'">
+                    <td>
+                      <img src="" alt="" width="50" height="50">
+                    </td>
+                    <td>$4</td>
+                    <td>4</td>
+                    <td>4</td>
+                    <td>4 kg</td>
+                    <td>4name</td>
+                    <td>c4</td>
+                    <td>This is a description...</td>
+                    <td>
+                        <button id="p4" style="position:relative; right:0px;"
+                        class="btn pull-right">
+                            View Product
+                        </button>
+                    </td>
+                </tr>
+              </div>
+            </tbody>
+            </table>
+            <hr>
+        </div>
             </div>
             <div class="span2">
                 <ul class="thumbnails">
@@ -86,27 +187,6 @@ $product = json_decode($product, true);
                 </div>-->
             </div>
         </div>
-    </div> <!-- /container -->
-    <hr>
-    <div class="container">
-        <h3>Related Products:</h3>
-        <div class="row-fluid">
-            <div class="span4">
-              <h4>Heading</h4>
-              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-              <p><a class="btn" href="#">View details &raquo;</a></p>
-            </div><!--/span-->
-            <div class="span4">
-              <h4>Heading</h4>
-              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-              <p><a class="btn" href="#">View details &raquo;</a></p>
-            </div><!--/span-->
-            <div class="span4">
-              <h4>Heading</h4>
-              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-              <p><a class="btn" href="#">View details &raquo;</a></p>
-            </div><!--/span-->
-        </div><!--/row-->
     </div> <!-- /container -->
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
