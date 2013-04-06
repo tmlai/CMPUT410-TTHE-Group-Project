ca924a93afdb36d940ca17c21699f9c88d1cbf00
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-06 17:06:59 -0600

    javascript caching?

--- source/view/elements/register.js
+++ source/view/elements/register.js
@@ -7,7 +7,7 @@ function checkRegistration() {
 	var email = document.getElementById("emailField").value;
 	var admin = document.getElementById("adminCheck").checked;
 	var adminCode = document.getElementById("adminCode").value;
-	alert(isEmpty(adminCode));
+	alert("admin code is empty:" + isEmpty(adminCode));
 	//check if input is valid
 	if(isEmpty(username) || isEmpty(password) || isEmpty(email)) {
 		alert("Please make sure all required fields are filled in.");
