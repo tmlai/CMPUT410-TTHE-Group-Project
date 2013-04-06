52cddd7d8dd645ac606bc96e7045821e60eb238b
Tully Johnson <tullyjohnson@gmail.com>
2013-04-06 11:56:29 -0600

    navbar update4

--- source/view/elements/navbar.php
+++ source/view/elements/navbar.php
@@ -54,15 +54,31 @@
             <li class="pull-right"><a href="#cart">View Cart    
                 <i class="icon-shopping-cart icon-white"></i></a></li>
             <li class="dropdown">
-              <?php
-                if($_SESSION['user'] != "") {
-                  // user dropdown
-                  //require($DOCUMENT_ROOT . "./elements/user_dropdown.php");
-                } else
-                  // sign in dropdown
-                  require($DOCUMENT_ROOT . "./elements/signin_dropdown.php");
-                }
-              ?>
+<a class="dropdown-toggle" 
+   data-toggle="dropdown" href="#">Sign In
+   <span class="caret"></span>
+ </a>
+<ul class="dropdown-menu">
+  <form name="loginform" class="form" 
+    enctype="text/plain" action="#" 
+    onSubmit="return checkLogin();">
+  <li>
+    <br>
+    <input type="text" id="usernameField" 
+	 placeholder="Username">
+    <input type="password" id="passwordField" 
+	 placeholder="Password">
+  </li>
+  <li class="text-center">
+    <button type="submit" 
+	 class="btn btn-primary">Sign in</button>
+    <button class="btn" 
+    onclick="location.href='register.php';return false;">
+	 Register
+    </button>
+  </li>
+  </form>
+</ul>
             </li>
             </ul>
         </div><!--/.nav-collapse -->
