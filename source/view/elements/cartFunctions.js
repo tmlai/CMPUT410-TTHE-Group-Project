4521ec6d226c6dff9216376d525c6c2efdf5e03b
Tully Johnson <tullyjohnson@gmail.com>
2013-04-06 19:11:46 -0600

    product update 18

--- source/view/elements/cartFunctions.js
+++ source/view/elements/cartFunctions.js
@@ -1,6 +1,16 @@
 // Add a product to the user's cart
 function addProdToCart(pid) {
   alert("checking stock...");
+  var xmlhttp = new XMLHttpRequest();
+	if (window.XMLHttpRequest)
+	{// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+  
   // Return if product is in stock
   xmlhttp.onreadystatechange=function() {
     if (xmlhttp.readyState==4 && xmlhttp.status==200) {
