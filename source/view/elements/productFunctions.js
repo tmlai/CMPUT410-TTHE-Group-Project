a7dfa1f5125379b8c53dc6ca203ec280fa374873
Tully Johnson <tullyjohnson@gmail.com>
2013-04-07 14:26:49 -0600

    product functs update 5 - test 1

--- source/view/elements/productFunctions.js
+++ source/view/elements/productFunctions.js
@@ -14,26 +14,26 @@ function checkInStock(pid) {
   
   // Return if product is in stock
   xmlhttp.onreadystatechange=function() {
-    document.getElementById("stockDiv").innerHTML = "readyState = " + xmlhttp.readyState;
+    //document.getElementById("stockDiv").innerHTML = "readyState = " + xmlhttp.readyState;
     if (xmlhttp.readyState==4 && xmlhttp.status==200) {
       alert("call came back!");
       //var jsonArray = JSON.parse(xmlhttp.responseText);
       document.getElementById("stockDiv").innerHTML = xmlhttp.responseText;
-      var orderBtn = document.getElementsByName("orderBtn");
-      orderBtn.style.visibility="visible";
+      var orderBtn = document.orderBtn;
+      orderBtn.style.visibility= "visible";
       if((jsonArray.quantity + getExternalAvail(pid)) == 0) {
         //document.getElementById("stockDiv").innerHTML = "(In Stock)";
       //} else {
         document.getElementById("stockDiv").innerHTML = "(Out of Stock)";
-        orderBtn.className="btn btn-danger";
-        orderBtn.innerHTML="Out of Stock";
+        orderBtn.className = "btn btn-danger";
+        orderBtn.innerHTML ="Out of Stock";
       } else {
         document.getElementById("stockDiv").innerHTML = "";
       }
     }
-  }
-  var xmlhttp=new XMLHttpRequest();
-  xmlhttp.open('GET', '/source/controller/ProductServices.php?id=' + pid, 'true');
+  };
+  //var xmlhttp=new XMLHttpRequest();
+  xmlhttp.open('GET', '/source/controller/ProductServices.php?id=' + pid, true);
   xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xmlhttp.send();
 }
