0724e6713ae5c15b9de30eb9794aa1c2b48cc70d
Tully Johnson <tullyjohnson@gmail.com>
2013-04-07 12:36:08 -0600

    cart functions and product functions update 1

--- source/view/elements/cartFunctions.js
+++ source/view/elements/cartFunctions.js
@@ -1,46 +1,89 @@
-// Add a product to the user's cart
+/*
+ * Add a product to the user's cart.
+ */
 function addProdToCart(pid) {
   alert("checking stock...");
   var xmlhttp = new XMLHttpRequest();
-	if (window.XMLHttpRequest)
-	{// code for IE7+, Firefox, Chrome, Opera, Safari
+	if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
 		xmlhttp=new XMLHttpRequest();
-	}
-	else
-	{// code for IE6, IE5
+	}	else {
+    // code for IE6, IE5
 		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
 	}
   
   // Return if product is in stock
   xmlhttp.onreadystatechange=function() {
     if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-      alert("debug: checking stock");
-      var jsonArray = JSON.parse(xmlhttp.responseText);
-      var cartJson = JSON.parse(readCookie('cart'));
-      //if(jsonArray.quantity == 0 ||  <) {
-      //  //checkExternalAvail(pid);
-      //  alert("debug: No Stock Available");
-      //} else {
-        
-        // Check if cart cookie exists
-        if(cartJson != null){
-          // add product to cart, else update qty in cart.
-          if(jsonArray[0][pid] == null) {
-            cartJson.push(',"pid":"1"')
-            createCookie('cart', JSON.stringify(cartJson), 0);
-          } else
-        } else {
-          // Create cart cookie storing product ids.
-          var cartCookie = '{"pid":"' + new Array(pid) + '"}';
-          cartCookie = JSON.stringify(cartCookie);
-          createCookie('cart', cartCookie, 0);
-        }
-        if(num == null) num = 0;
-        createCookie('cart_' + pid, num + 1, 0);
-      //}
+      updateProductQty(pid);
     }
   }
   var xmlhttp=new XMLHttpRequest();
   xmlhttp.open('GET', '/source/controller/ProductServices.php?id=' + pid, 'true');
   xmlhttp.send();
+}
+
+/*
+ * Add quantity of a product.
+ * Note: JSON format of cart: {"pid":["quantity", "from external store quantity"]}
+ * Default qty is 1. When greater than 1, the quantity is the total not
+ * to increment quantity.
+ * @return  true/false on availability of quantity of a product.
+ */
+function updateCartProductQty(pid, qty = 1) {
+  alert("debug: checking stock");
+  var jsonArray = JSON.parse(xmlhttp.responseText);
+  var cartJson = JSON.parse(readCookie('cart'));
+  // For quantity used from other stores.
+  var qtyExternal = 0;
+  
+  // Check if cart cookie exists
+  if(cartJson != null){
+      if(jsonArray.quantity < qty) {
+        // Check external stores for remaining quantity
+        qtyExternal = checkExternalAvail(pid, qty - jsonArray.quantity);
+        var qtyAvail = qtyExternal + jsonArray.quantity;
+        if(qytAvail < qty) {
+          var qtyBool = confirm("Only " + qtyAvail + " of this product(" + pid 
+            + ") is available, would you like to purchase this amount?");
+          if(qtyBool) {
+            qty = qtyAvail;
+          } else {
+            alert("Sorry we did not have the quantity you requested, please take"
+              + " a look at our Top Ranked Related Products for other options.");
+            return(false;)
+          }
+        }
+      }
+      // Add pid entry with quantity to cart.
+        cartJson[0][pid] = new Array(qty, qtyExternal);
+      // if(jsonArray.quantity >= qtyNew) {
+        // update the quantity.
+        // cartJson[0][pid] = new Array(qty, qtyExternal);
+        // jsonArray.quantity -= qtyNew;
+      // } else {
+        // Check other stores for remaining quantity.
+        // var qtyRem = qty - jsonArray.quantity;
+        // checkExternalAvail(pid, qtyRem);
+      // }
+    }
+  } else {
+    // Create cart and store this product id.
+    cartJson = '{"' + pid + '":"' + new Array(qty, qtyExternal) + '"}';
+  }
+  // Update the cart
+  cartJson = JSON.stringify(cartJson);
+  createCookie('cart', cartJson, 0);
+  return true;
+}
+
+/* 
+ * Search external stores for pid of a given quantity.
+ * default qty is 1.
+ * Return quantity of availability.
+ */
+function getExternalAvail(pid, qty = 1) {
+  // DEBUG: to be implemented
+  alert("debug: Checking other stores...");
+  return 1;
 }
---
+++ source/view/elements/productFunctions.js
@@ -0,0 +1,26 @@
+/*
+ * Check if product is available (quantity > 0).
+ * @return  true/false if available
+ */
+function checkAvailability(pid) {
+  /*alert("checking stock...");
+  var xmlhttp = new XMLHttpRequest();
+	if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}	else {
+    // code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+  
+  // Return if product is in stock
+  xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+      updateProductQty(pid);
+    }
+  }
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.open('GET', '/source/controller/ProductServices.php?id=' + pid, 'true');
+  xmlhttp.send();
+  */
+}
