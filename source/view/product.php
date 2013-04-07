0443e7b9856db884f8461a3c9442bdc1e535f607
Tully Johnson <tullyjohnson@gmail.com>
2013-04-07 14:18:31 -0600

    product functs update 4 - test 14

--- source/view/product.php
+++ source/view/product.php
@@ -24,8 +24,47 @@ $product = json_decode($product, true);
     <link href="elements/rateit/src/rateit.css" rel="stylesheet">
     <script type="text/javascript" language="JavaScript" 
       src="./elements/cartFunctions.js"></script>
-    <script type="text/javascript" language="JavaScript" 
-      src="./elements/productFunctions.js">
+    <script type="text/javascript" language="JavaScript" >
+      
+      /*
+ * Check if product is available (quantity > 0).
+ * @return  true/false if available
+ */
+function checkInStock(pid) {
+  var xmlhttp = new XMLHttpRequest();
+	if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}	else {
+    // code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+  
+  // Return if product is in stock
+  xmlhttp.onreadystatechange=function() {
+    document.getElementById("stockDiv").innerHTML = "readyState = " + xmlhttp.readyState;
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+      alert("call came back!");
+      //var jsonArray = JSON.parse(xmlhttp.responseText);
+      document.getElementById("stockDiv").innerHTML = xmlhttp.responseText;
+      var orderBtn = document.getElementsByName("orderBtn");
+      orderBtn.style.visibility="visible";
+      if((jsonArray.quantity + getExternalAvail(pid)) == 0) {
+        //document.getElementById("stockDiv").innerHTML = "(In Stock)";
+      //} else {
+        document.getElementById("stockDiv").innerHTML = "(Out of Stock)";
+        orderBtn.className="btn btn-danger";
+        orderBtn.innerHTML="Out of Stock";
+      } else {
+        document.getElementById("stockDiv").innerHTML = "";
+      }
+    }
+  }
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.open('GET', '/source/controller/ProductServices.php?id=' + pid, 'true');
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlhttp.send();
+}
     </script>
   </head>
   <body onLoad="checkInStock(<?php echo "'" . $_SESSION['productID'] . "'";?>);">
