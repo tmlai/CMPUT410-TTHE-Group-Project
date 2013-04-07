7ebe213f15b37e2e09442ff5fe6f7f9b7e2ed34e
Tully Johnson <tullyjohnson@gmail.com>
2013-04-07 13:37:00 -0600

    product functs update 4 -test 2

--- source/view/product.php
+++ source/view/product.php
@@ -25,7 +25,41 @@ $product = json_decode($product, true);
     <script type="text/javascript" language="JavaScript" 
       src="./elements/cartFunctions.js"></script>
     <script type="text/javascript" language="JavaScript" 
-      src="./elements/productFunctions.js"></script>
+      >
+        /*
+ * Check if product is available (quantity > 0).
+ * @return  true/false if available
+ */
+function checkInStock(pid) {
+  var xmlhttp = new XMLHttpRequest();
+	if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}	else {
+    // code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+  
+  // Return if product is in stock
+  xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+      var jsonArray = JSON.parse(xmlhttp.responseText);
+      //if((jsonArray.quantity + getExternalAvail(pid)) == 0) {
+        //document.getElementById("stockDiv").innerHTML = "(In Stock)";
+      //} else {
+        //document.getElementById("stockDiv").innerHTML = "(Out of Stock)";
+        alert("call came back!");
+        var orderBtn = document.getElementsByName("orderBtn");
+        orderBtn.className="btn btn-danger";
+        orderBtn.innerHTML="Out of Stock";
+      //}
+    }
+  }
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.open('GET', '/source/controller/ProductServices.php?id=' + pid, 'true');
+  xmlhttp.send();
+}
+    </script>
   </head>
   <body onLoad="checkInStock(<?php echo "'" . $_SESSION['productID'] . "'";?>);">
     <?php
