0cc67dfcafc2b7ce7fcc72fe40382d2f40902589
Tully Johnson <tullyjohnson@gmail.com>
2013-04-07 17:47:26 -0600

    prod and signin update .11

--- source/view/product.php
+++ source/view/product.php
@@ -41,21 +41,6 @@ $product = json_decode($product, true);
         require("./elements/navbar_signin.php");
       }
     ?>
-<div class="row-fluid">
-  <div class="span12">
-    Fluid 12
-    <div class="row-fluid">
-      <div class="span6">
-        Fluid 6
-        <div class="row-fluid">
-          <div class="span6">Fluid 6</div>
-          <div class="span6">Fluid 6</div>
-        </div>
-      </div>
-      <div class="span6">Fluid 6</div>
-    </div>
-  </div>
-</div>
     <div class="container-fluid">
         <div class="row-fluid">
             <div class="span9">
