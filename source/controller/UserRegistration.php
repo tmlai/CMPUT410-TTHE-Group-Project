18ecee660d1dcc52ad73d823e38a611d0b1cd312
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-06 19:56:33 -0600

    debugging

--- source/controller/UserRegistration.php
+++ source/controller/UserRegistration.php
@@ -1,26 +1,10 @@
 <?php 
-namespace controller;
+//namespace controller;
 use model\DbLayer;
 
 include_once '../model/DbLayer.php';
 include_once '../model/Customer.php';
 
-// try {
-	// $dbLayer = new DbLayer();
-    // $requestMethod = strtolower($_SERVER['REQUEST_METHOD']);
-	// switch($requestMethod) {
-		// case 'get':
-			// echo "Wrong Request Method. Please use 'POST'.";
-			// break;
-		// case 'post':
-			// $userJsonString = file_get_contents("php://input");
-			
-			// var_dump($userJsonString);
-			
-	// }
-// } catch (Exception $e) {
-    // echo 'Caught exception: ',  $e->getMessage(), "\n";
-// }
 ?>
 <html>
 	<header>
