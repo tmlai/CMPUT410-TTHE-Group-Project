8a7a00833cd29ef66db91b287cbcc3e263c218e5
Tully Johnson <tullyjohnson@gmail.com>
2013-04-07 15:39:46 -0600

    product update 7.5

--- source/view/elements/productFunctions.js
+++ source/view/elements/productFunctions.js
@@ -60,7 +60,7 @@ function checkInStock(pid) {
       // DEBUG: need to be json to use buildRelatedProducts
       //buildRelatedProducts(jsonArray);
       // DEBUG: temporary, see two lines above
-      document.getElementById("resultsDiv") = jsonArray;
+      document.getElementById("resultsDiv").innerHTML = jsonArray;
     }
   };
   xmlhttp.open('GET', '/source/controller/ProductServices.php?ranknum=5&rankcat=' 
