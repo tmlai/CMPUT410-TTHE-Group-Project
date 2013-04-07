4ed9b56b3faca7ac66051ee333c09a35085d421b
Tully Johnson <tullyjohnson@gmail.com>
2013-04-07 14:34:36 -0600

    product functs update 6
    Checks for availability for product on load

--- source/view/elements/productFunctions.js
+++ source/view/elements/productFunctions.js
@@ -19,17 +19,12 @@ function checkInStock(pid) {
       var orderBtn = document.getElementById("orderBtn");
       orderBtn.style.visibility= "visible";
       if((jsonArray.quantity + getExternalAvail(pid)) > 0) {
-        //document.getElementById("stockDiv").innerHTML = "(In Stock)";
-      //} else {
-        document.getElementById("stockDiv").innerHTML = "(Out of Stock)";
         orderBtn.className = "btn btn-danger";
         orderBtn.innerHTML ="Out of Stock";
-      } else {
-        document.getElementById("stockDiv").innerHTML = "";
       }
+      document.getElementById("stockDiv").innerHTML = "";
     }
   };
-  //var xmlhttp=new XMLHttpRequest();
   xmlhttp.open('GET', '/source/controller/ProductServices.php?id=' + pid, true);
   xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xmlhttp.send();
