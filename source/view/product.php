ee466ddccba9ab3ab013f68ec2e538938b6be50d
Tully Johnson <tullyjohnson@gmail.com>
2013-04-07 15:29:53 -0600

    product update 7.1

--- source/view/product.php
+++ source/view/product.php
@@ -75,74 +75,7 @@ $product = json_decode($product, true);
                 </thead>
                 <tbody>
                   <div id="resultsDiv">
-                    <tr onclick="location.href='./product.php?id=1'">
-                        <td>1</td>
-                        <td>
-                          <img src="" alt="" width="50" height="50">
-                        </td>
-                        <td>$1</td>
-                        <td>1 kg</td>
-                        <td>1name</td>                        
-                        <td>c1</td>
-                        <td>This is a description...</td>
-                        <td>
-                            <button id="p1" style="position:relative; right:0px;"
-                            class="btn pull-right">
-                                View Product
-                            </button>
-                        </td>
-                    </tr>
-                    <tr onclick="location.href='./product.php?id=2'">
-                        <td>2</td>
-                        <td>
-                          <img src="" alt="" width="50" height="50">
-                        </td>
-                        <td>$2</td>
-                        <td>2 kg</td>
-                        <td>2name</td>
-                        <td>c2</td>
-                        <td>This is a description...</td>
-                        <td>
-                            <button id="p2" style="position:relative; right:0px;"
-                            class="btn pull-right">
-                                View Product
-                            </button>
-                        </td>
-                    </tr>
-                    <tr onclick="location.href='./product.php?id=3'">
-                        <td>3</td>
-                        <td>
-                          <img src="" alt="" width="50" height="50">
-                        </td>
-                        <td>$3</td>
-                        <td>3 kg</td>
-                        <td>3name</td>
-                        <td>c3</td>
-                        <td>This is a description...</td>
-                        <td>
-                            <button id="p3" style="position:relative; right:0px;"
-                            class="btn pull-right">
-                                View Product
-                            </button>
-                        </td>
-                    </tr>
-                    <tr onclick="location.href='./product.php?id=4'">
-                        <td>4</td>
-                        <td>
-                          <img src="" alt="" width="50" height="50">
-                        </td>
-                        <td>$4</td>
-                        <td>4 kg</td>
-                        <td>4name</td>
-                        <td>c4</td>
-                        <td>This is a description...</td>
-                        <td>
-                            <button id="p4" style="position:relative; right:0px;"
-                            class="btn pull-right">
-                                View Product
-                            </button>
-                        </td>
-                    </tr>
+                    
                   </div>
                 </tbody>
                 </table>
