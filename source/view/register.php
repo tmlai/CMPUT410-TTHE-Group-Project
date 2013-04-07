6cccc27853c407ec1b17cc868e0885837697ac32
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-06 19:07:38 -0600

    test

--- source/view/register.php
+++ source/view/register.php
@@ -43,10 +43,10 @@ session_start();
       // Navigation Bar
       if($_SESSION['user'] != "") {
         // user dropdown
-        require($DOCUMENT_ROOT . "./elements/navbar_user.php");
+        require("/elements/navbar_user.php");
       } else {
         // sign in dropdown
-        require($DOCUMENT_ROOT . "./elements/navbar_signin.php");
+        require("/elements/navbar_signin.php");
       }
     ?>
     <div class="container">
