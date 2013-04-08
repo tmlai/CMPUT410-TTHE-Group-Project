443b0d4030d41b72a8940498d82baa781b5c4f14
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-08 17:54:10 -0600

    debugging server error

--- source/controller/Search.php
+++ source/controller/Search.php
@@ -9,10 +9,10 @@ $requestMethod = strtolower($_SERVER['REQUEST_METHOD']);
 
 if($requestMethod == "get") {
 	$search = $_GET['searchField'];
-	$dbLayer = new DbLayer();
-	$list = $dbLayer->searchProduct($search);
-	// var_dump($search);
-	echo "into if statement";
+	// $dbLayer = new DbLayer();
+	// $list = $dbLayer->searchProduct($search);
+	// // var_dump($search);
+	echo $search;
 }
 echo "out of if";
 ?>
