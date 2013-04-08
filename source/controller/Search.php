b880c4e9d87b7487c75bd75d166ec37ed8155378
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-08 17:35:19 -0600

    debug

--- source/controller/Search.php
+++ source/controller/Search.php
@@ -13,7 +13,8 @@ if($requestMethod == "get") {
 	$search = $_GET['searchField'];
 	$dbLayer = new DbLayer();
 	$list = $dbLayer->searchProduct($search);
-	var_dump($search);
+	// var_dump($search);
+	echo "into if statement";
 }
 //echo \json_encode($list);
 
