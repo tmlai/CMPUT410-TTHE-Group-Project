65d367c38ca90567c6c6bd518a65a99230b95c26
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-07 21:38:02 -0600

    added trim

--- source/controller/Login.php
+++ source/controller/Login.php
@@ -9,30 +9,29 @@ $_SESSION['prevPage'] = $_SESSION['prevPage'];
 $requestMethod = strtolower($_SERVER['REQUEST_METHOD']);
 
 if($requestMethod == "post") {
-	$jsonString = file_get_contents("php://input");
-	$json = json_decode($jsonString, true);
-	var_dump($json);
+	try {
+		$jsonString = file_get_contents("php://input");
+		$json = json_decode($jsonString, true);
+		
+		$username = trim($json['username']);
+		$password = trim($json['password']);
+		
+		//query for a user with that name
+		$dbLayer = new DbLayer();
+		$status = $$dbLayer->authenticateCustomer($username, $password);
+		if($status == 0) {
+			echo "failed";
+		} else if($status == 1) {
+			echo "failed";
+		} else if($status == 2) {
+			echo "success";
+		} else {
+			echo "error";
+		}
+	} catch (Exception $e) {
+		echo "error";
+	}
+} else {
+	echo "error."
 }
-
-
-// if($requestMethod == "post") {
-	// try {
-		// //query for a user with that name
-		// $dbLayer = new DbLayer();
-		// $status = $$dbLayer->authenticateCustomer($username, $password);
-		// if($status == 0) {
-			// echo "failed";
-		// } else if($status == 1) {
-			// echo "failed";
-		// } else if($status == 2) {
-			// echo "success";
-		// } else {
-			// echo "error";
-		// }
-	// } catch (Exception $e) {
-		// echo "error";
-	// }
-// } else {
-	// echo "error."
-// }
 ?>
