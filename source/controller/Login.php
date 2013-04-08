5e8e4c9ada10dcd4e0eec781b7fd41e1760c95bb
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-07 21:53:55 -0600

    debug4

--- source/controller/Login.php
+++ source/controller/Login.php
@@ -1,40 +1,41 @@
 <?php
 //namespace controller;
 
-//use model\DbLayer;
+use model\DbLayer;
 
 session_start();
 
 $_SESSION['prevPage'] = $_SESSION['prevPage'];
 $requestMethod = strtolower($_SERVER['REQUEST_METHOD']);
-echo "Before if";
-if($requestMethod == "post") {
-	try {
-		echo "In the try";
-		$jsonString = file_get_contents("php://input");
-		$json = json_decode($jsonString, true);
+echo $requestMethod;
+// echo "Before if";
+// if($requestMethod == "post") {
+	// try {
+		// echo "In the try";
+		// $jsonString = file_get_contents("php://input");
+		// $json = json_decode($jsonString, true);
 		
-		//$username = trim($json['username']);
-		//$password = trim($json['password']);
+		// //$username = trim($json['username']);
+		// //$password = trim($json['password']);
 
-		//query for a user with that name
-		//$dbLayer = new DbLayer();
-		//$status = $$dbLayer->authenticateCustomer($username, $password);
+		// //query for a user with that name
+		// //$dbLayer = new DbLayer();
+		// //$status = $$dbLayer->authenticateCustomer($username, $password);
 		
-		echo "before status";
-		// if($status == 0) {
-			// echo "failed";
-		// } else if($status == 1) {
-			// echo "failed";
-		// } else if($status == 2) {
-			// echo "success";
-		// } else {
-			// echo "error";
-		// }
-	} catch (Exception $e) {
-		echo "error";
-	}
-} else {
-	echo "error."
-}
+		// echo "before status";
+		// // if($status == 0) {
+			// // echo "failed";
+		// // } else if($status == 1) {
+			// // echo "failed";
+		// // } else if($status == 2) {
+			// // echo "success";
+		// // } else {
+			// // echo "error";
+		// // }
+	// } catch (Exception $e) {
+		// echo "error";
+	// }
+// } else {
+	// echo "error."
+// }
 ?>
