fa3786f5e59e142b20ddb608a0e503f1c5d97f8d
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-07 21:48:22 -0600

    debug3

--- source/controller/Login.php
+++ source/controller/Login.php
@@ -7,28 +7,30 @@ session_start();
 
 $_SESSION['prevPage'] = $_SESSION['prevPage'];
 $requestMethod = strtolower($_SERVER['REQUEST_METHOD']);
-
+echo "Before if";
 if($requestMethod == "post") {
 	try {
+		echo "In the try";
 		$jsonString = file_get_contents("php://input");
 		$json = json_decode($jsonString, true);
 		
 		//$username = trim($json['username']);
 		//$password = trim($json['password']);
-		
-		$status = 0;
+
 		//query for a user with that name
 		//$dbLayer = new DbLayer();
 		//$status = $$dbLayer->authenticateCustomer($username, $password);
-		if($status == 0) {
-			echo "failed";
-		} else if($status == 1) {
-			echo "failed";
-		} else if($status == 2) {
-			echo "success";
-		} else {
-			echo "error";
-		}
+		
+		echo "before status";
+		// if($status == 0) {
+			// echo "failed";
+		// } else if($status == 1) {
+			// echo "failed";
+		// } else if($status == 2) {
+			// echo "success";
+		// } else {
+			// echo "error";
+		// }
 	} catch (Exception $e) {
 		echo "error";
 	}
