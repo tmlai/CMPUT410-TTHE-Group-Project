a1226b3e5f1fb252f4eeab36e5fdd96594b4cd18
Tully Johnson <tullyjohnson@gmail.com>
2013-04-08 13:11:51 -0600

    cart funct 2.9 - debug 1

--- source/view/elements/productFunctions.js
+++ source/view/elements/productFunctions.js
@@ -154,6 +154,7 @@ function rateProduct(pid, user, value) {
     if (xmlhttp.readyState==4 && xmlhttp.status==200) {
       var jsonObj = JSON.parse(xmlhttp.responseText);
       alert("cart DEBUG: getting one product: " + jsonObj);
+      console.log(jsonObj);
       //var jsonArray = xmlhttp.responseText;
       return(jsonObj);
     }
