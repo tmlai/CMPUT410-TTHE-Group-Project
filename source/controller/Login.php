0ce2fa24d1472f8a1e7aef0d6732a182c9303481
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-07 22:23:25 -0600

    debug13

--- source/controller/Login.php
+++ source/controller/Login.php
@@ -11,23 +11,15 @@ $_SESSION['prevPage'] = $_SESSION['prevPage'];
 $requestMethod = strtolower($_SERVER['REQUEST_METHOD']);
 
 if($requestMethod == "post") {
-	try {
-		$jsonString = file_get_contents("php://input");
-		$json = json_decode($jsonString, true);
-		
-		$username = trim($json['username']);
-		$password = trim($json['password']);
-
-		//echo "username: $username, password: $password";
-		
-		//query for a user with that name
-		$dbLayer = new DbLayer();
-		$status = $dbLayer->authenticateCustomer($username, $password);
-		echo strval($status);
-	} catch (Exception $e) {
-		echo "error";
-	}
-} else {
-	echo "error."
+	$jsonString = file_get_contents("php://input");
+	$json = json_decode($jsonString, true);
+	
+	$username = trim($json['username']);
+	$password = trim($json['password']);
+	
+	//query for a user with that name
+	$dbLayer = new DbLayer();
+	$status = $dbLayer->authenticateCustomer($username, $password);
+	echo strval($status);
 }
 ?>
