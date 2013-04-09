e5f82a7c135f5789bf8354c55db856a8f462543c
Tully Johnson <tullyjohnson@gmail.com>
2013-04-08 22:13:17 -0600

    index.php update 2

--- source/view/index.php
+++ source/view/index.php
@@ -14,7 +14,7 @@ $_SESSION['prevPage'] = $_SERVER['REQUEST_URI'];
     ?>
     <link href="bootstrap/css/carousel.css" rel="stylesheet">
   </head>
-  <body onLoad="getCategoies()">   
+  <body onLoad="getCategories()">   
     <?php
       // Navigation Bar
       if($_SESSION['user'] != "") {
