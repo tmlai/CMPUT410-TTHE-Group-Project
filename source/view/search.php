dfa447376d5341ea15054162c741b01949e8a3a3
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-09 14:11:40 -0600

    testing imports..

--- source/view/search.php
+++ source/view/search.php
@@ -25,10 +25,10 @@ $advanced = $_GET['advanced'];
       // Navigation Bar
       if($_SESSION['user'] != "") {
         // user dropdown
-        require($DOCUMENT_ROOT . "./elements/navbar_user.php");
+        require("/elements/navbar_user.php");
       } else {
         // sign in dropdown
-        require($DOCUMENT_ROOT . "./elements/navbar_signin.php");
+        require("/elements/navbar_signin.php");
       }
     ?>
     <div class="container">
