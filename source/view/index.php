131cecb68c1010b85add2596358214657d98bd65
Tully Johnson <tullyjohnson@gmail.com>
2013-04-09 13:40:02 -0600

    index carouself test

--- source/view/index.php
+++ source/view/index.php
@@ -22,14 +22,18 @@ $_SESSION['prevPage'] = $_SERVER['REQUEST_URI'];
                      
         // This initialises carousels on the container elements specified, in this case, carousel1.
         $("#carousel1").CloudCarousel(		
-          {			
+          {
+            reflHeight: 56,
+            reflGap:2,
             xPos: 128,
             yPos: 32,
             buttonLeft: $("#left-but"),
             buttonRight: $("#right-but"),
             altBox: $("#alt-text"),
             titleBox: $("#title-text"),
-            autoRotate: 'right'
+            autoRotate: 'left',
+            autoRotateDelay: 1200,
+            speed:0.2
           }
         );
       });
@@ -53,20 +57,19 @@ $_SESSION['prevPage'] = $_SERVER['REQUEST_URI'];
             <img src="./elements/img/spinner.gif" alt="...Loading...">
       </div>
       <!-- This is the container for the carousel. -->
-        <div id = "carousel1" style="width:500px; height:250px;background:#000;overflow:scroll;">            
-            <p id="title-text"></p>
-            <p id="alt-text"></p>
-            <!-- All images with class of "cloudcarousel" will be turned into carousel items -->
-            <!-- You can place links around these images -->
-            <img class = "cloudcarousel" src="/img/products/c000020.jpg" height="100" width="100" alt="Flag 1 Description" title="Flag 1 Title" />
-            <img class = "cloudcarousel" src="/img/products/c000019.jpg" height="100" width="100" alt="Flag 2 Description" title="Flag 2 Title" />
-            <img class = "cloudcarousel" src="/img/products/c000018.jpg" height="100" width="100" alt="Flag 3 Description" title="Flag 3 Title" />
-            <img class = "cloudcarousel" src="/img/products/c000017.jpg" height="100" width="100" alt="Flag 4 Description" title="Flag 4 Title" />
-        </div>
-        
-        <!-- Define left and right buttons. -->
-        <button id="left-but"  type="button" value="Left"><i class="icon-arrow-left"></i></button>
-        <button id="right-but" type="button" value="Right"><i class="icon-arrow-Right"></i></button>
+      <div id = "carousel1" style="width:600px; height:395px;background:#000;overflow:scroll;">            
+          <div id="title-text"></div>
+          <div id="alt-text"></div>
+          <!-- All images with class of "cloudcarousel" will be turned into carousel items -->
+          <!-- You can place links around these images -->
+          <img class = "cloudcarousel" src="/img/products/c000020.jpg" height="100" width="100" alt="Flag 1 Description" title="Flag 1 Title" />
+          <img class = "cloudcarousel" src="/img/products/c000019.jpg" height="100" width="100" alt="Flag 2 Description" title="Flag 2 Title" />
+          <img class = "cloudcarousel" src="/img/products/c000018.jpg" height="100" width="100" alt="Flag 3 Description" title="Flag 3 Title" />
+          <img class = "cloudcarousel" src="/img/products/c000017.jpg" height="100" width="100" alt="Flag 4 Description" title="Flag 4 Title" />  
+      
+        <button id="left-but" class="carouselLeft" style="position:absolute;top:20px;right:64px;"></button>
+        <button id="right-but" class="carouselRight" style="position:absolute;top:20px;right:20px;"></button>      
+      </div>
     </div> <!-- /container -->
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
