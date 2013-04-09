8d564598925731260f76170f604e0c041721e2b3
Tri Lai <tmlai@ualberta.ca>
2013-04-08 18:32:25 -0600

    Commit to send to Tully

--- source/model/Recommender/RateProduct.php
+++ source/model/Recommender/RateProduct.php
@@ -1,82 +1,84 @@
 <?php 
-// echo "before include<br/>";
+use model\DbLayer;
+
 include_once ('../DbLayer.php');
-include_once ('../UserRatingProduct');
+include_once ('../UserRatingProduct.php');
 
-// echo "b4 get request method<br/>";
-// $requestMethod = $_SERVER['REQUEST_METHOD'];
+echo "b4 get request method<br/>";
+$requestMethod = $_SERVER['REQUEST_METHOD'];
 
-// echo "before get user from session<br/>";
-// $userName = $_SESSION["user"]; // from session
-// echo "after get user from session".$userName."<br/>";
+echo "before get user from session<br/>";
+$userName = $_SESSION["user"]; // from session
+echo "after get user from session".$userName."<br/>";
 
-// if ($requestMethod == "POST"){
+if ($requestMethod == "POST"){
 	
-// 	if (!isset($userName) || $userName === ""){
-// 		$message = array(
-// 				"status" => "Failed",
-// 				"message" => "You have to login"
-// 		);
-// 	}
-// 	else {
-// 		$productId = $_POST["productId"];
-// 		$rating = $_POST["rating"];
+	if (!isset($userName) || $userName === ""){
+		$message = array(
+				"status" => "Failed",
+				"message" => "You have to login"
+		);
+	}
+	else {
+		$productId = $_POST["productId"];
+		$rating = $_POST["rating"];
 		
-// 		$result = DbLayer.php::rateProduct(new UserRatingProduct($userName,$productId,$rating));
-// 		$message = array();
-// 		if ($result){
-// 			$message = array(
-// 				"status" => "Ok",
-// 				"message" => "Your rating is updated." 	
-// 			);
-// 		}
-// 		else{
-// 			$message = array(
-// 			"status" => "Failed",
-// 			"message" => "Your rating could not be updated. Please try again."
-// 			);
-// 		}
-// 	}
-// 	echo json_encode($message);
-// }
-// elseif ($requestMethod == "GET"){
-// 	$cat = $_GET["category"];
-// 	echo "Cat".$cat."<br/>";
-// 	$n = $_GET["n"];
-// 	echo "n ".$n."<br/>";
+		$result = DbLayer.php::rateProduct(new UserRatingProduct($userName,$productId,$rating));
+		$message = array();
+		if ($result){
+			$message = array(
+				"status" => "Ok",
+				"message" => "Your rating is updated." 	
+			);
+		}
+		else{
+			$message = array(
+			"status" => "Failed",
+			"message" => "Your rating could not be updated. Please try again."
+			);
+		}
+	}
+	echo json_encode($message);
+}
+elseif ($requestMethod == "GET"){
+	$cat = $_GET["category"];
+	echo "Cat".$cat."<br/>";
+	$n = $_GET["n"];
+	echo "n ".$n."<br/>";
 // 	if (!isset($cat) || trim($cat) === ""){
 		$cat = null;
-// 	}
-// 	if (!isset($n) || trim($n) === ""){
+	}
+	if (!isset($n) || trim($n) === ""){
 		$n = 5;
-// 	}
-// 	echo "Cat".$cat."<br/>";
-// 	echo "n ".$n."<br/>";
+	}
+	echo "Cat".$cat."<br/>";
+	echo "n ".$n."<br/>";
 	try {
 		echo "b4";
-		$productList = DbLayer::recommendRelatedProducts($n, $cat);
+		$dbLayer = new DbLayer();
+		$productList = $dbLayer->recommendRelatedProducts($n, $cat);
 		echo "after";
 	}
 	catch (Exception $e){
 		echo $e.getMessage()."<br/>";
 	}
 	
-// 	echo "productList array".$productList."<br/>";
-// 	$productJSONList = array();
-// 	echo "iterate through the array<br/>";
-// 	foreach ($productList as $product){
-// 		$simpleProduct = array (
-// 			"cid" => $product.getCid(),
-// 			"name" => $product.getName(),
-// 			"price" => $product.getPrice(),
-// 			"image" => $product.getImage(),
-// 			"description" => $product.getDescription()
-// 		);
-// 		echo "before encoding<br/>";
-// 		$simpleProductJSON = json_encode($simpleProduct	);
-// 		echo "after encoding<br/>";
-// 		$productJSONList[] =$simpleProductJSON;
-// 	}
-// 	echo "json array".$productJsonList."<br/>";
-// 	echo json_encode($productJSONList);
-// }
+	echo "productList array".$productList."<br/>";
+	$productJSONList = array();
+	echo "iterate through the array<br/>";
+	foreach ($productList as $product){
+		$simpleProduct = array (
+			"cid" => $product.getCid(),
+			"name" => $product.getName(),
+			"price" => $product.getPrice(),
+			"image" => $product.getImage(),
+			"description" => $product.getDescription()
+		);
+		echo "before encoding<br/>";
+		$simpleProductJSON = json_encode($simpleProduct	);
+		echo "after encoding<br/>";
+		$productJSONList[] =$simpleProductJSON;
+	}
+	echo "json array".$productJsonList."<br/>";
+	echo json_encode($productJSONList);
+}
