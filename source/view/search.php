04ffd05a62c880d982cbeb1dc3c8a8b694eb1ec5
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-09 00:14:50 -0600

    fixed chevron?

--- source/view/search.php
+++ source/view/search.php
@@ -54,11 +54,13 @@ $advanced = $_GET['advanced'];
       if(dropBool) {
         dropBool = false;
         document.getElementById("dropDiv").innerHTML=
-          "Advanced Search  <i class=\"icon-chevron-up\"></i>";
+          'Advanced Search  <i class="icon-chevron-up" data-toggle="collapse"'
+            + ' data-target="#advSearch"></i>';
       } else {
         dropBool = true;
         document.getElementById("dropDiv").innerHTML=
-          "Advanced Search  <i class=\"icon-chevron-down\"></i>";
+          'Advanced Search  <i class="icon-chevron-down" data-toggle="collapse"'
+            + ' data-target="#advSearch"></i>';
       }
     }
 	
