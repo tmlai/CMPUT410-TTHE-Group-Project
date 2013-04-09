c6c8dd545a5ac1ec69140f010c584049a63ce03e
Tully Johnson <tullyjohnson@gmail.com>
2013-04-09 16:26:46 -0600

    index php no container div

--- source/view/index.php
+++ source/view/index.php
@@ -25,13 +25,11 @@ $_SESSION['prevPage'] = $_SERVER['REQUEST_URI'];
         require("./elements/navbar_signin.php");
       }
     ?>
-    <div class="container">
-      <div id="carouselElement">
-      </div>
-      <div id="categoryContainerDiv">
-        <div id="loadingSpinner" align="center" style="visibility:visible">
-            <img src="./elements/img/spinner.gif" alt="...Loading...">
-        </div>
+    <div id="carouselElement">
+    </div>
+    <div id="categoryContainerDiv">
+      <div id="loadingSpinner" align="center" style="visibility:visible">
+          <img src="./elements/img/spinner.gif" alt="...Loading...">
       </div>
     </div>
     <script src="http://code.jquery.com/jquery.js"></script>
