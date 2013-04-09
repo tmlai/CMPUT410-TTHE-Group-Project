913ad46a456d91ecf00e1f122452250ecbd2417f
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-08 22:38:37 -0600

    log search object

--- source/view/search.php
+++ source/view/search.php
@@ -146,7 +146,7 @@ $advanced = $_GET['advanced'];
 			//make json
 			var jsonSearch = JSON.stringify(search);
 			
-			alert(search);
+			console.log(search);
 			var xmlhttp = new XMLHttpRequest();
 			if (window.XMLHttpRequest) {
 			// code for IE7+, Firefox, Chrome, Opera, Safari
