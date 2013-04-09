22ad3b92681fa0693e66c78bcae7c90fc29a5efe
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-08 23:27:22 -0600

    dump list returned

--- source/controller/AdvSearch.php
+++ source/controller/AdvSearch.php
@@ -10,24 +10,53 @@ $requestMethod = strtolower($_SERVER['REQUEST_METHOD']);
 $jsonSearch = file_get_contents("php://input");
 $search = json_decode($jsonSearch, true);
 if($requestMethod == "post") {
-	// $name = trim()
-	// $code = document.getElementById('searchCodeField').value.trim;
-	// $category = document.getElementById('searchCategoryField').value.trim;
-	// $priceFrom = document.getElementById('priceFromField').value.trim;
-	// $priceTo = document.getElementById('priceToField').value.trim;
-	// $minQty = document.getElementById('minQtyField').value.trim;
-	// $maxQty = document.getElementById('maxQtyField').value.trim;
-	// $minWeight = document.getElementById('minWeightField').value.trim;
-	// $maxWeight = document.getElementById('maxWeightField').value.trim;
-	var_dump($search);
+	$name = trim($search['name']);
+	$code = trim($search['code']);
+	$category = trim($search['category']);
+	$priceFrom = trim($search['priceFrom']);
+	$priceTo = trim($search['priceTo']);
+	$minQty = trim($search['minQty']);
+	$maxQty = trim($search['maxQty']);
+	$minWeight = trim($search['minWeight']);
+	$maxWeight = trim($search['maxWeight']);
+
+	//if empty set as null
+	if($name == "") {
+		$name = null;
+	}
+	if($code == "") {
+		$code = null;
+	}
+	if($category == "") {
+		$category = null;
+	}
+	if($priceFrom == "") {
+		$priceFrom = null;
+	}
+	if($priceTo == "") {
+		$priceTo = null;
+	}
+	if($minQty == "") {
+		$minQty = null;
+	}
+	if($maxQty == "") {
+		$maxQty = null;
+	}
+	if($minWeight == "") {
+		$minWeight = null;
+	}
+	if($maxWeight == "") {
+		$maxWeight = null;
+	}
+	//build arrays for ranges
+	$priceRange = {$priceFrom, $priceTo};
+	$availRange = {$minQty, $maxQty};
+	$weightRange = {$minWeight, $maxWeight};
 	
-	
-	
-	
-	
-	
-	// $dbLayer = new DbLayer();
-	// $list = $dbLayer->searchProductByName($partial);
+	$dbLayer = new DbLayer();
+	$list = $dbLayer->searchProductByConstraints(
+		$name, $code, $category, $priceRange, $availRange, $weightRange );
+	var_dump($list);
 	// $allReturned = array();
 	// for($i=0; $i<count($list);$i++) {
 		// $singleProduct = $list[$i];
