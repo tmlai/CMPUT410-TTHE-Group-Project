eac11f845e8ff203ee2cb7931cb0efa9cb1288e2
Edwin Chung <edwin.chung@ualberta.ca>
2013-04-08 22:30:18 -0600

    debug statement

--- source/view/search.php
+++ source/view/search.php
@@ -145,7 +145,7 @@ $advanced = $_GET['advanced'];
 			
 			//make json
 			var jsonSearch = JSON.stringify(search);
-			
+			alert(jsonSearch);
 			var xmlhttp = new XMLHttpRequest();
 			if (window.XMLHttpRequest) {
 			// code for IE7+, Firefox, Chrome, Opera, Safari
