a634ab88fea122f37f59227cd6d0628f0f542382
Tully Johnson <tullyjohnson@gmail.com>
2013-04-09 23:55:25 -0600

    title of pages updated

--- source/view/cart.php
+++ source/view/cart.php
@@ -6,7 +6,7 @@ $_SESSION['prevPage'] = $_SERVER['REQUEST_URI'];
 <!DOCTYPE html>
 <html>
   <head>
-    <title>TTHE Enterprise - Search Results</title>
+    <title>TTHE Enterprise - View Cart</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" language="JavaScript" 
       src="./elements/productFunctions.js"></script>
